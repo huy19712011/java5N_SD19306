fd40a9ab34a3bab819408b78d9ee61f3f5f2f677
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2025-01-19 09:12:47 +0700

    JpaRepository: part 1

--- src/main/java/com/example/java5n_sd19306/Java5NSd19306Application.java
+++ src/main/java/com/example/java5n_sd19306/Java5NSd19306Application.java
@@ -1,10 +1,20 @@
 package com.example.java5n_sd19306;
 
+import com.example.java5n_sd19306.entity.Student;
+import com.example.java5n_sd19306.repository.StudentRepositoryV2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Java5NSd19306Application {
+public class Java5NSd19306Application implements CommandLineRunner {
+
+    private final StudentRepositoryV2 studentRepositoryV2;
+
+    public Java5NSd19306Application(StudentRepositoryV2 studentRepositoryV2) {
+        this.studentRepositoryV2 = studentRepositoryV2;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Java5NSd19306Application.class, args);
@@ -12,4 +22,17 @@ public class Java5NSd19306Application {
         System.out.println("running...");
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+        // 2. example
+        studentRepositoryV2
+                .findByNameContaining("student")
+                .forEach(s -> System.out.println(s.getName()));
+
+        // 3.
+        studentRepositoryV2
+                .findByNameOrIdV3b("student 1", 1004)
+                .forEach(s -> System.out.println(s.getName()));
+    }
 }
--- src/main/java/com/example/java5n_sd19306/repository/StudentRepositoryV2.java
+++ src/main/java/com/example/java5n_sd19306/repository/StudentRepositoryV2.java
@@ -2,8 +2,35 @@ package com.example.java5n_sd19306.repository;
 
 import com.example.java5n_sd19306.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface StudentRepositoryV2 extends JpaRepository<Student, Long> {
 
     // all methods
+    // 1. Built-in methods
+    // extends JpaRepository => ListPagingAndSortingRepository => PagingAndSortingRepository =>
+    // CrudRepository
+
+    // 2. Ghep ten cac truong => methods
+    // read...By, get...By, query...By, find...By
+    // First, Top: FindFirst2ByName, findTop10ByName
+    // Distinct: findDistinctByName
+    // AND, OR: findByNameOrDescription, findByNameAndPrice
+    // example: like/containing
+    List<Student> findByNameContaining(String name);
+
+
+    // 3. JPQL/ Native query with named parameters
+    // using index params
+    @Query("SELECT s FROM Student s WHERE s.name=?1 OR s.id=?2")
+    List<Student> findByNameOrIdV3a(String name, long id);
+
+    // using named params
+    @Query(value = "SELECT * FROM students s WHERE s.name=:name OR s.id=:id", nativeQuery = true)
+    List<Student> findByNameOrIdV3b(String name, long id);
+
+
+    // 4. Named queries
 }
