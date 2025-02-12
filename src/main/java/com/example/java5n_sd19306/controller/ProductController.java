43fee88a21dc787b76320f232e74ab323eeddbe6
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2025-02-12 08:50:03 +0700

    CRUD with multiple tables, dropdown list, ...: part 2

--- src/main/java/com/example/java5n_sd19306/controller/ProductController.java
+++ src/main/java/com/example/java5n_sd19306/controller/ProductController.java
@@ -3,9 +3,14 @@ package com.example.java5n_sd19306.controller;
 import com.example.java5n_sd19306.entity.Product;
 import com.example.java5n_sd19306.service.CategoryService;
 import com.example.java5n_sd19306.service.ProductService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -42,5 +47,47 @@ public class ProductController {
         model.addAttribute("categories", categoryService.getAllCategories());
 
         return "views/new_product";
+    }
+
+    @PostMapping("/products/saveProduct")
+    public String saveProduct(@Valid @ModelAttribute("product") Product product, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return "views/new_product";
+        }
+
+        productService.saveProduct(product);
+
+        return "redirect:/products";
+    }
+
+    @GetMapping("/products/deleteProduct/{id}")
+    public String deleteProduct(@PathVariable("id") long id) {
+        productService.deleteProductById(id);
+        return "redirect:/products";
+    }
+
+    @GetMapping("/products/showFormForUpdate/{id}")
+    public String showFormForUpdate(@PathVariable("id") long id, Model model) {
+
+        Product product = productService.getProductById(id);
+
+        model.addAttribute("product", product);
+        model.addAttribute("categories", categoryService.getAllCategories());
+
+        return "views/update_product";
+    }
+
+    @PostMapping("/products/updateProduct")
+    public String updateProduct(@Valid @ModelAttribute("products") Product product, BindingResult bindingResult, Model model) {
+
+        if(bindingResult.hasErrors()) {
+            model.addAttribute("categories", categoryService.getAllCategories());
+            return "views/update_product";
+        }
+
+        productService.updateProduct(product);
+
+        return "redirect:/products";
     }
 }
--- src/main/java/com/example/java5n_sd19306/service/ProductService.java
+++ src/main/java/com/example/java5n_sd19306/service/ProductService.java
@@ -2,6 +2,7 @@ package com.example.java5n_sd19306.service;
 
 import com.example.java5n_sd19306.entity.Product;
 import com.example.java5n_sd19306.repository.ProductRepository;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,5 +19,25 @@ public class ProductService {
     public List<Product> getAllProducts() {
 
         return productRepository.findAll();
+    }
+
+    public void saveProduct(@Valid Product product) {
+
+        productRepository.save(product);
+    }
+
+    public void deleteProductById(long id) {
+
+        productRepository.deleteById(id);
+    }
+
+    public Product getProductById(long id) {
+
+        return productRepository.findById(id).get();
+    }
+
+    public void updateProduct(@Valid Product product) {
+
+        productRepository.save(product);
     }
 }
